6b01fd5019b0d8d77a6cbe0b796c4be675ebdbf1
shakespear95 <59427923+shakespear95@users.noreply.github.com>
2022-11-12 12:46:32 +0800

    finished the menu section

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const hamburger = document.querySelector(".hamburger");
 const navul = document.querySelector(".nav-ul");
+const cancelbutton =document.querySelector(".cancelbutton");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navul.classList.toggle("active");
+    cancelbutton.classList.toggle("active");
 })
 
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
