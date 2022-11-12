7351fab627cc8e5cc6ae6f10c1f1cc6fb076622f
shakespear95 <59427923+shakespear95@users.noreply.github.com>
2022-11-12 22:57:42 +0800

    managed to finish putting the lower bar on the website

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const hamburger = document.querySelector(".hamburger");
 const navul = document.querySelector(".nav-ul");
-const cancelbutton =document.querySelector(".cancelbutton");
+const bottom = document.querySelector(".nav-list2");
+
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navul.classList.toggle("active");
-    cancelbutton.classList.toggle("active");
+    bottom.classList.toggle("active");
+
 })
 
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
