0da23161258ee0f9ae51e4b95e1bb5011016374e
shakespear95 <59427923+shakespear95@users.noreply.github.com>
2022-11-15 00:39:52 +0800

    finished the pop up buttons

--- script.js
+++ script.js
@@ -2,17 +2,57 @@ const hamburger = document.querySelector('.hamburger');
 const navul = document.querySelector('.nav-ul');
 const bottom = document.querySelector('.nav-link2');
 const popButton = document.querySelectorAll('.work-button');
+const popButton1 = document.querySelectorAll('.work-button1');
 const popModal = document.querySelector('.buttonmob');
-const closeButton = document.querySelector('.close-button');
+const popModaldesk = document.querySelector('.buttondesk');
+const closeButton = document.querySelectorAll('.close-button');
 const Overlay = document.querySelector('.active');
-closeButton.addEventListener('click', () => {
+const workSection = document.querySelector('.work');
+const homeSection = document.querySelector('.home');
+const aboutsection = document.querySelector('.about-myself');
+const contactform = document.querySelector('.form-container');
+const headerpart = document.querySelector('header');
+
+
+
+closeButton.forEach((x) => (
+  x.addEventListener('click', () => {
   popModal.classList.toggle('active');
-});
+  popModaldesk.classList.toggle('active');
+  workSection.classList.toggle('active');
+  homeSection.classList.toggle('active');
+  aboutsection.classList.toggle('active');
+  contactform.classList.toggle('active');
+  headerpart.classList.toggle('active');
+  })
+));
+
+// closeButton.addEventListener('click', () => {
+//   popModal.classList.toggle('active');
+//   popModaldesk.classList.toggle('active');
+//   console.log('Button Clicked');
+// });
+
 popButton.forEach((x) => (
   x.addEventListener('click', () => {
   // Do something if any of the four button is clicked
     popModal.classList.toggle('active');
     Overlay.classList.toggle('active');
+    popModaldesk.classList.toggle('active');
+    workSection.classList.toggle('active');
+    homeSection.classList.toggle('active');
+    aboutsection.classList.toggle('active');
+    contactform.classList.toggle('active');
+    headerpart.classList.toggle('active');
+
+  })
+));
+popButton1.forEach((x) => (
+  x.addEventListener('click', () => {
+  // Do something if any of the four button is clicked
+    popModaldesk.classList.toggle('active');
+    Overlay.classList.toggle('active');
+    popModaldesk.classList.toggle('active');
   })
 ));
 hamburger.addEventListener('click', () => {
